53f35c26b79d7e24463bed712a42a89aa98a8eb2
yannis216 <yannisbeuke@googlemail.com>
2019-01-04 16:46:17 +0100

    Activity_recipe layout added

---
+++ app/src/main/java/com/example/android/bakingapp/Activities/RecipeActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.bakingapp.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.bakingapp.R;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+}
