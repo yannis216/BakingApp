0cb92e1b883c86ec31408f2eac5707c3c9d36a9e
yannis216 <yannisbeuke@googlemail.com>
2019-01-07 12:31:59 +0100

    Exoplayer Workinggit add --all But one bug when click on last item in some lists

--- app/src/main/java/com/example/android/bakingapp/Activities/StepActivity.java
+++ app/src/main/java/com/example/android/bakingapp/Activities/StepActivity.java
@@ -2,14 +2,24 @@ package com.example.android.bakingapp.Activities;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.android.bakingapp.Models.Recipe;
 import com.example.android.bakingapp.Models.Step;
 import com.example.android.bakingapp.Models.StepViewModel;
 import com.example.android.bakingapp.R;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import java.util.List;
 
@@ -18,6 +28,8 @@ public class StepActivity extends AppCompatActivity {
     private Integer currentStepId;
     private Recipe recipe;
     private StepViewModel viewModel;
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
 
 
     @Override
@@ -26,6 +38,7 @@ public class StepActivity extends AppCompatActivity {
         setContentView(R.layout.activity_step);
 
         TextView tvStepLongDesc = findViewById(R.id.tv_longDesc);
+        playerView = findViewById(R.id.pv_Step);
 
 
         // Get Recipe and CurrentStepId from ViewModel or Intent
@@ -43,10 +56,51 @@ public class StepActivity extends AppCompatActivity {
 
         List<Step> steps = recipe.getSteps();
         Step currentStep = steps.get(currentStepId);
+        String url = currentStep.getVideoURL();
+
+        if (url != ""){
+            setupExoPlayer(url);
+        }
+        else{
+            playerView.setVisibility(View.GONE);
+        }
+
+
+
 
         tvStepLongDesc.setText(currentStep.getDescription());
 
+    }
 
 
+    private void setupExoPlayer(String url){
+        Uri videoUri = Uri.parse(url);
+        player = ExoPlayerFactory.newSimpleInstance(this);
+        playerView.setPlayer(player);
+
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "BakingApp"));
+        MediaSource videoSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(videoUri);
+        player.prepare(videoSource);
+    }
+
+    //TODO Call ExoPlayer.release somewhere (but where?)
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(player != null){
+            player.release();
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(player != null){
+            player.release();
+        }
     }
 }
